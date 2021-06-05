35a9109f4d487d8d85cf2122c530cb2a22178be1
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-05 11:48:27 +0200

    Update pocketmine-setup.sh

--- files/pocketmine-setup.sh
+++ files/pocketmine-setup.sh
@@ -5,7 +5,7 @@ wget -q -O - https://get.pmmp.io | bash -s -
 mkdir plugins
 cd plugins
 echo Installing BuilderTools plugin dev-148..
-wget https://poggit.pmmp.io/r/126779/BuilderTools_dev-148.phar
+wget https://poggit.pmmp.io/r/126779/BuilderTools_dev-148.phar -q
 cd ..
 clear
 echo Done!
