862c70c88d0751439d741c0e0da080232f2ca91c
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-05 11:51:46 +0200

    Update bedrock-setup.sh

--- files/bedrock-setup.sh
+++ files/bedrock-setup.sh
@@ -1,6 +1,6 @@
 mkdir bedrock
 cd bedrock
-echo Getting MCPE 1.16.221.01
+echo Getting MCPE 1.16.221.01..
 wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.16.221.01.zip -q
 unzip bedrock-server-1.16.221.01.zip
 chmod +x bedrock_server
