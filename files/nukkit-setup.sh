4dc802bc09eaaa036e0ffb7a108e3f1910fddf30
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-05 11:49:35 +0200

    Update nukkit-setup.sh

--- files/nukkit-setup.sh
+++ files/nukkit-setup.sh
@@ -1,7 +1,7 @@
 clear
 mkdir nukkit
 cd nukkit
-wget https://ci.opencollab.dev/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+wget https://ci.opencollab.dev/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar -q
 cd ..
 echo java -jar nukkit-1.0-SNAPSHOT.jar >> run-server.sh
 chmod +x run-server.sh
