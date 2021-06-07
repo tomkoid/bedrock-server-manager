d7d4b0a97b0a2c9e8e09e63983d804f857dbea90
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-07 14:51:52 +0200

    Update uninstall.sh

--- files/uninstall.sh
+++ files/uninstall.sh
@@ -1,10 +1,15 @@
 echo WARNING: All data will be deleted!
 read -p "Press any key to continue.."
 clear
+echo Deleting directories..
 rm -r Bedrock
 rm -r PocketMine
 rm -r Nukkit
 rm installall.sh
 rm uninstall.sh
-clear
+echo Modifying aliases..
+unalias bedrock
+unalias pocketmine
+unalias nukkit
+unalias uninstall_bedrock
 echo Done!
