d658933e348161f51d26dcd823b74d0baccdf322
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-14 17:27:39 +0200

    Update installall.sh

--- files/installall.sh
+++ files/installall.sh
@@ -1,12 +1,12 @@
 echo Install Bedrock, PocketMine and Nukkit?
 read -p "Press any key to continue.."
 cd Bedrock
-./bedrock-setup.sh
+source bedrock-setup.sh
 cd ..
 cd PocketMine
-./pocketmine-setup.sh
+source pocketmine-setup.sh
 cd ..
 cd Nukkit
-./nukkit-setup.sh
+source nukkit-setup.sh
 clear
 echo All installed!
